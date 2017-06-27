9f42e673dec94280f5d5da48e5e87bf3f2997a2c
Franco Bulgarelli <franco@mumuki.org>
2017-06-26 23:32:57 -0300

    Partially supporting extended format

--- spec/inspection_spec.rb
+++ spec/inspection_spec.rb
@@ -1,10 +1,16 @@
 require_relative './spec_helper'
 
-include Mumukit
 
-describe Inspection do
-  it { expect(Inspection.parse('HasBinding').to_h).to eq(type: 'HasBinding', negated: false, target: nil) }
-  it { expect(Inspection.parse('Not:HasBinding').to_h).to eq(type: 'HasBinding', negated: true, target: nil) }
-  it { expect(Inspection.parse('HasUsage:m').to_h).to eq(type: 'HasUsage', negated: false, target: 'm') }
-  it { expect(Inspection.parse('Not:HasUsage:m').to_h).to eq(type: 'HasUsage', negated: true, target: 'm') }
+describe Mumukit::Inspection do
+  it { expect(Mumukit::Inspection.parse('HasBinding').to_h).to eq(type: 'HasBinding', negated: false) }
+  it { expect(Mumukit::Inspection.parse('Not:HasBinding').to_h).to eq(type: 'HasBinding', negated: true) }
+  it { expect(Mumukit::Inspection.parse('HasUsage:m').to_h).to eq(type: 'HasUsage', negated: false, target: 'm') }
+  it { expect(Mumukit::Inspection.parse('Not:HasUsage:m').to_h).to eq(type: 'HasUsage', negated: true, target: 'm') }
+
+  it { expect(Mumukit::Inspection.parse('HasUsage:=m').to_h).to eq(type: 'HasUsage', negated: true, target: 'm') }
+  it { expect(Mumukit::Inspection.parse('HasUsage:~m').to_h).to eq(type: 'HasUsage', negated: true, target: 'm', like: true) }
+  it { expect(Mumukit::Inspection.parse('HasUsage:*').to_h).to eq(type: 'HasUsage', negated: true) }
+
+  it { expect(Mumukit::Inspection.parse_binding_name('foo')).to eq 'foo' }
+  it { expect(Mumukit::Inspection.parse_binding_name('Intransitive:foo')).to eq 'foo' }
 end
