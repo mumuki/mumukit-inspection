87970c5668c55bd60de7f1c83576b957980180a4
Franco Bulgarelli <franco@mumuki.org>
2017-06-26 23:36:38 -0300

    Adding more 18n specs

--- spec/i18n_spec.rb
+++ spec/i18n_spec.rb
@@ -16,9 +16,19 @@ describe Mumukit::Inspection::I18n do
 
   context 'es locale' do
     before { I18n.locale = :es }
+    it { expect(translate_expectation('', 'HasBinding:foo')).to eq('<strong>foo</strong> debe estar definido') }
+    it { expect(translate_expectation('', 'DeclaresClass:foo')).to eq('la clase <strong>foo</strong> debe estar definida') }
+    it { expect(translate_expectation('foo', 'DeclaresClass')).to eq('la clase <strong>foo</strong> debe estar definida') }
 
     it { expect(translate_expectation('foo', 'HasBinding')).to eq('<strong>foo</strong> debe estar definido') }
     it { expect(translate_expectation('foo', 'Not:HasUsage:baz')).to eq('<strong>foo</strong> no debe utilizar <strong>baz</strong>') }
     it { expect(translate_expectation('foo', 'Not:HasLambda')).to eq('<strong>foo</strong> no debe emplear expresiones lambda') }
+
+    it { expect(translate_expectation('Intransitive:foo', 'Not:HasLambda')).to eq('<strong>foo</strong> no debe emplear expresiones lambda') }
+
+    it { expect(translate_expectation('foo', 'Not:HasUsage:=baz')).to eq('<strong>foo</strong> no debe utilizar <strong>baz</strong>') }
+    it { expect(translate_expectation('foo', 'Not:HasUsage:~baz')).to eq('<strong>foo</strong> no debe utilizar nada parecido a <strong>baz</strong>') }
+    it { expect(translate_expectation('foo', 'Not:HasUsage:*')).to eq('<strong>foo</strong> no debe utilizar nada') }
+    it { expect(translate_expectation('foo', 'Not:HasUsage')).to eq('<strong>foo</strong> no debe utilizar nada') }
   end
 end
