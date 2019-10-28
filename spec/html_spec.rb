98b8ebc46866e973b3e72b1c7d34a31be0bd45e6
Lucho Cannavo <luchotc@hotmail.com.ar>
2019-10-28 18:57:30 -0300

    Adding failing test with rel

--- spec/html_spec.rb
+++ spec/html_spec.rb
@@ -5,13 +5,25 @@ describe Mumukit::Inspection::Html do
   after { Mulang::Inspection.unregister_extension! Mumukit::Inspection::Html }
 
   describe 'parsing' do
-    let(:inspection_s) { 'DeclaresAttribute:href="https://mumuki.org"' }
+    context 'with href' do
+      let(:inspection_s) { 'DeclaresAttribute:href="https://mumuki.org"' }
 
-    it { expect(Mulang::Inspection.parse(inspection_s)).to json_like(type: 'DeclaresAttribute',
-                                                                     target: { type: :unknown, value: 'href="https://mumuki.org"' },
-                                                                     negated: false,
-                                                                     i18n_namespace: "mumukit.inspection") }
-    it { expect(Mulang::Inspection.parse(inspection_s).to_s).to eq inspection_s }
+      it { expect(Mulang::Inspection.parse(inspection_s)).to json_like(type: 'DeclaresAttribute',
+                                                                       target: { type: :unknown, value: 'href="https://mumuki.org"' },
+                                                                       negated: false,
+                                                                       i18n_namespace: "mumukit.inspection") }
+      it { expect(Mulang::Inspection.parse(inspection_s).to_s).to eq inspection_s }
+    end
+
+    context 'with src' do
+      let(:inspection_s) { 'DeclaresAttribute:src="https://mumuki.org"' }
+
+      it { expect(Mulang::Inspection.parse(inspection_s)).to json_like(type: 'DeclaresAttribute',
+                                                                       target: { type: :unknown, value: 'src="https://mumuki.org"' },
+                                                                       negated: false,
+                                                                       i18n_namespace: "mumukit.inspection") }
+      it { expect(Mulang::Inspection.parse(inspection_s).to_s).to eq inspection_s }
+    end
   end
 
   describe 'i18n' do
